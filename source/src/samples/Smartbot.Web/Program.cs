c81ee4aaf14e0aae34ee71feea5869887685c29f
Korsneso <johnkors@gmail.com>
2019-08-13 07:55:50 +0200

    Reference Slackbot.Net from Web

--- source/src/samples/Smartbot.Web/Program.cs
+++ source/src/samples/Smartbot.Web/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Slackbot.Net;
 
 namespace Smartbot.Web
 {
