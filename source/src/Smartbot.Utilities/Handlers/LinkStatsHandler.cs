f865004e5b45772536ca6aab687da6778d123915
Korsneso <johnkors@gmail.com>
2020-01-09 16:28:47 +0100

    Avoid www. when comparing urls

--- source/src/Smartbot.Utilities/Handlers/LinkStatsHandler.cs
+++ source/src/Smartbot.Utilities/Handlers/LinkStatsHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Slackbot.Net.Abstractions.Handlers;
 using Slackbot.Net.Abstractions.Handlers.Models.Rtm.MessageReceived;
 using Slackbot.Net.Abstractions.Publishers;
@@ -15,12 +16,14 @@ namespace Smartbot.Utilities.Handlers
         private readonly IEnumerable<IPublisher> _publishers;
         private readonly ISlackClient _slackClient;
         private readonly SlackMessagesStorage _storage;
+        private readonly ILogger<LinkStatsHandler> _logger;
 
-        public LinkStatsHandler(IEnumerable<IPublisher> publishers, ISlackClient slackClient, SlackMessagesStorage storage)
+        public LinkStatsHandler(IEnumerable<IPublisher> publishers, ISlackClient slackClient, SlackMessagesStorage storage, ILogger<LinkStatsHandler> logger)
         {
             _publishers = publishers;
             _slackClient = slackClient;
             _storage = storage;
+            _logger = logger;
         }
 
         public bool ShouldShowInHelp => true;
@@ -52,7 +55,7 @@ namespace Smartbot.Utilities.Handlers
                     {
                         var domains = urlsForSmarting
                             .Select(c => new Uri(c.Url))
-                            .GroupBy(u => u.Host);
+                            .GroupBy(GetComparableDomain);
                         
                         domains = domains.Where(o => o.Count() > 1).OrderByDescending(o => o.Count()).Take(5);
 
@@ -75,7 +78,7 @@ namespace Smartbot.Utilities.Handlers
                     var allUrls = await _storage.GetAllUrls();
                     var domains = allUrls
                         .Select(c => new Uri(c.Url))
-                        .GroupBy(u => u.Host);
+                        .GroupBy(GetComparableDomain);
                         
                     domains = domains.Where(o => o.Count() > 1).OrderByDescending(o => o.Count()).Take(10);
                     var text = "Alle:";
@@ -97,6 +100,26 @@ namespace Smartbot.Utilities.Handlers
             }
 
             return new HandleResponse("OK");
+        }
+
+        private string GetComparableDomain(Uri u)
+        {
+            var s = u.Host;
+
+            if (u.Host.StartsWith("www."))
+            {
+                try
+                {
+                    s = u.Host.Substring(4, u.Host.Length - 4);
+                }
+                catch (Exception)
+                {
+                    _logger.LogError($"{u.Host} could not be trimmed");
+                }
+            }
+                
+            
+            return s;
         }
 
         private async Task Publish(SlackMessage message, string text)
