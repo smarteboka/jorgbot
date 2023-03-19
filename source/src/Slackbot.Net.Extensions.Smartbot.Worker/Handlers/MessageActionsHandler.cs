7b69c90545cd988586b8d7971add4da866469deb
John Korsnes <johnkors@gmail.com>
2023-03-19 05:08:46 +0100

    Tjuning

--- source/src/Slackbot.Net.Extensions.Smartbot.Worker/Handlers/MessageActionsHandler.cs
+++ source/src/Slackbot.Net.Extensions.Smartbot.Worker/Handlers/MessageActionsHandler.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -17,11 +20,17 @@ public class MessageActionsHandler : IHandleMessageActions
 {
     private readonly ISlackClient _slackClient;
     private readonly ILogger<MessageActionsHandler> _logger;
+    private readonly OpenAIClient _client;
+    private readonly HttpClient _httpClient;
 
     public MessageActionsHandler(ISlackClient slackClient, ILogger<MessageActionsHandler> logger)
     {
         _slackClient = slackClient;
         _logger = logger;
+        var key = Environment.GetEnvironmentVariable("SMARTBOT_OPENAI_KEY");
+        _client = new OpenAIClient(key);
+        _httpClient = new HttpClient();
+
     }
     
     public Task<EventHandledResponse> Handle(MessageActionInteraction @event)
@@ -29,29 +38,46 @@ public class MessageActionsHandler : IHandleMessageActions
         _logger.LogInformation(JsonConvert.SerializeObject(@event));
         Task.Run(async () =>
         {
-            var key = Environment.GetEnvironmentVariable("SMARTBOT_OPENAI_KEY");
-            var client = new OpenAIClient(key);
-
             var completionPrompt = @event.Callback_Id switch
             {
-                "gpt_critico" => ElCritico(@event.Message.Text),
+                "gpt_critico" => await ElCritico(@event.Message.Text, @event.Message.User),
                 "gpt_tldr" => await ThreadTLDR(@event.Channel.Id, @event.Message_Ts),
                 _ => null
             };
 
             if (completionPrompt != null)
             {
-                var res = await client.ChatEndpoint.GetCompletionAsync(new ChatRequest(completionPrompt));
-                var completions = res.FirstChoice.Message.Content;
-
-                await _slackClient.ChatPostMessage(new ChatPostMessageRequest
+                var ctoken = new CancellationTokenSource(10000);
+                try
                 {
-                    Channel = @event.Channel.Id,
-                    thread_ts = @event.Message_Ts,
-                    Text = completions,
-                    Link_Names = true,
-                    Reply_Broadcast = true
-                });         
+                    var res = await _client.ChatEndpoint.GetCompletionAsync(new ChatRequest(messages: completionPrompt),
+                        ctoken.Token);
+                    _logger.LogInformation(res.ProcessingTime.ToPrettyFormat());
+                    var completions = res.FirstChoice.Message.Content;
+
+                    await _slackClient.ChatPostMessage(new ChatPostMessageRequest
+                    {
+                        Channel = @event.Channel.Id,
+                        thread_ts = @event.Message_Ts,
+                        Text = completions,
+                        Link_Names = true,
+                        Reply_Broadcast = true
+                    });
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, e.Message);
+
+                    var txt = e switch
+                    {
+                        TaskCanceledException or {InnerException: TaskCanceledException} => "⏳ Oops, nå brukte jeg litt lang tid her. Prøv igjen litt senere, a.",
+                        _ => $"Der gikk noe i stå-bot. Smartbot må nok inn på sørvis."
+                    };
+                    await _httpClient.PostAsJsonAsync(@event.Response_Url, new
+                    {
+                        text = txt
+                    });
+                }
             }
             else
             {
@@ -62,13 +88,19 @@ public class MessageActionsHandler : IHandleMessageActions
         return Task.FromResult(new EventHandledResponse("OK"));
     }
 
-    private static IEnumerable<ChatPrompt> ElCritico(string text)
+    private async Task<IEnumerable<ChatPrompt>> ElCritico(string text, string userId)
     {
-        yield return new ChatPrompt("user", $"Jeg vil at du skal late som du er en satiriker. Jeg vil gi deg" +
-                                            $"en tekst, som du skal forklare i en sarkastisk tone og belyse ulike temaer" +
-                                            $"i lys av dagens tid. Dra paraleller til store historiske hendelser og personer. Bruk maks 100 ord. \n\n" +
-                                            $"Teksten du skal forklare: \n" +
-                                            $"{text}");
+        var usersInReplies = await _slackClient.UsersList();
+        var allMembers = usersInReplies.Members;
+        var userForId = allMembers.FirstOrDefault(u => u.Id == userId);
+        var p1 = new ChatPrompt("system", "Du er en satiriker som liker å bruke ironi og sarkasme i dialog. " +
+                                          "Du liker å bruke ungdommelig språk og starter og avslutter alle setninger med emojiis. " +
+                                          "Spesielt hodeskalle-emojii. Du bruker maaaaange hodeskalle-emojii " +
+                                          "Du bruker max 80 ord i svarene dine. " +
+                                          "Du liker å sammenligne ting mot de største hendelsene i verdenshistorien."+
+                                          "Du er som regel negativ og et dystert syn på alle tema du får servert. ");
+        var p2 = new ChatPrompt("user", $"{userForId} sier:{text}");
+        return new[] { p1, p2 };
     }
     
     private async Task<IEnumerable<ChatPrompt>> ThreadTLDR(string channel, string ts)
@@ -78,12 +110,13 @@ public class MessageActionsHandler : IHandleMessageActions
         var usersInReplies = await _slackClient.UsersList();
         var allMembers = usersInReplies.Members;
         _logger.LogInformation("kom hit2");
-        var chatDialog = replies.Messages.Select(c => $"{allMembers.FirstOrDefault(a => a.Id == c.User)?.Name ?? "En smarting"} : '{c.Text}'\n");
+        var chatDialog = replies.Messages.Select(c => $"{allMembers.FirstOrDefault(a => a.Id == c.User)?.Name ?? "inntrenger"} : '{c.Text}'\n");
         _logger.LogInformation(string.Join("\n", chatDialog));
         _logger.LogInformation("kom hit3");
-        var p1 = new ChatPrompt("system", "Du er en filmkritiker som elsker å anmelde meldingsutvekslinger mellom venner med satire");
+        var p1 = new ChatPrompt("system", "Du er en filmkritiker som elsker å anmelde meldingsutvekslinger mellom venner med satire. " +
+                                          "Du bruker sarkasme, ironi, språklige bilder og metaforer.");
         var p2 = new ChatPrompt("user",
-            "Du er en filmkritiker som elsker å anmelde meldingsutvekslinger mellom venner med satire.\n"+
+            // "Du er en filmkritiker som elsker å anmelde meldingsutvekslinger mellom venner med satire.\n"+
             "Jeg vil at du skal tolke en dialog mellom venner\n." +
             "Oppsummer samtalen i mellom disse vennene i et referat på maks 80 ord. " +
             "Svar på norsk. " +
@@ -100,7 +133,7 @@ public class MessageActionsHandler : IHandleMessageActions
             " \"Terningkast 0\".\n"+
             " \"Terningkast 1\".\n"+
             " \"Terningkast 3\".\n"+
-            "Film-manus-dialog:\n\n"+
+            "Dialog:\n\n"+
             $"{string.Join("\n", chatDialog)}");
 
         return new[] { p1, p2 };
