fc49c5db100317bcda1fb9955eb6470d766a32ed
John Korsnes <johnkors@gmail.com>
2024-01-29 15:18:42 +0100

    Fix app env issue

--- source/src/Smartbot.Web/Program.cs
+++ source/src/Smartbot.Web/Program.cs
@@ -3,9 +3,8 @@ using Smartbot.Web.App;
 
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
 var port = environment == "Production" ? Environment.GetEnvironmentVariable("PORT") : "1337";
-var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls($"http://*:{port}");
-builder.Host.UseEnvironment(environment);
 builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddAuthentication().AddSlackbotEvents(c => { c.SigningSecret = "123"; });
 builder.Services.AddSmartbot(builder.Configuration);
