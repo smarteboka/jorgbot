6976d9a14753bf125d8ea0e62e04f6e44476e985
John Korsnes <johnkors@gmail.com>
2024-01-29 15:24:37 +0100

    Add fly.toml to sln

--- source/src/Smartbot.Web/Program.cs
+++ source/src/Smartbot.Web/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Slackbot.Net.Endpoints.Authentication;
 using Smartbot.Web.App;
 
+
+var builder = WebApplication.CreateBuilder(args);
+// See fly.toml for the PORT environment variable & services > internal_port
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
 var port = environment == "Production" ? Environment.GetEnvironmentVariable("PORT") : "1337";
-var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls($"http://*:{port}");
 builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddAuthentication().AddSlackbotEvents(c => { c.SigningSecret = "123"; });
